79ffb39d934da02a96f3a7bb7ea79110c42efa49
minhee <minhee810@naver.com>
2023-11-23 15:51:04 +0900

    최신화 전

--- src/components/Study/StudyRecruitment.jsx
+++ src/components/Study/StudyRecruitment.jsx
@@ -2,7 +2,6 @@ import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import DateTimePicker from "../Fix/DateTimePicker";
-import MultiSelect from "./MultiSelect";
 import { data } from "autoprefixer";
 
 export default function StudyRecruitment() {
