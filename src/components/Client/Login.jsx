ec8427e5d583b8da31cff83347b59fac035a8815
dollae <96718120+dahyunhan@users.noreply.github.com>
2023-11-29 16:28:01 +0900

    Update Login.jsx

--- src/components/Client/Login.jsx
+++ src/components/Client/Login.jsx
@@ -15,7 +15,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:8081/api/login", {
+      const response = await axios.post("http://52.79.108.89:8080/api/login", {
         username,
         password,
       });
