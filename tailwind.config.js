4e115be68684566af9d726cc4c0c2fa6fb7b3f6b
dahyunhan <dollae7020@gmail.com>
2023-12-06 15:23:51 +0900

    1차 배포

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
-=======
   content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
@@ -22,4 +13,3 @@ module.exports = {
   }
 
   
->>>>>>> ecf5ce19f8f23913a4ee61f58f7f275c79b32f82
