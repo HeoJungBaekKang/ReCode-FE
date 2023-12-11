e2ec82d3167c3dd0178f83467a180528117852a0
Chan <chc6823@naver.com>
2023-12-11 16:47:22 +0900

    temp save

--- src/components/Chat/chatApp.jsx
+++ src/components/Chat/chatApp.jsx
@@ -39,10 +39,18 @@ const ChatApp = () => {
   };
 
   return (
+    <>
+     <div className="flex items-center p-4 border-b border-gray-300">
+        <div className="flex flex-col">
+          <span className="font-semibold">John Doe</span>
+        </div>
+      </div>
     <div>
-      <MessageList messages={messages} username={nickname} />
-      <MessageInput onSendMessage={addMessage} />
+    <MessageList messages={messages} username={nickname} />
+    <MessageInput onSendMessage={addMessage} />
     </div>
+
+    </>
   );
 };
 
