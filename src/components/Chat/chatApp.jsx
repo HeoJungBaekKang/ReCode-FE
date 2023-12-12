306be0742686e7af489ed19f1d5f03961ab1d0b2
Chan <chc6823@naver.com>
2023-12-12 17:37:51 +0900

    chat on progress

--- src/components/Chat/chatApp.jsx
+++ src/components/Chat/chatApp.jsx
@@ -1,97 +1,146 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import "./style.css";
+
 
 const ChatApp = () => {
   const {nickname,chatRoomId} = useParams();
-  const [messages, setMessages] = useState([]); // messages 변수를 빈 배열로 초기화
 
   useEffect(() => {
+    //===
+    const initMyMessage =  (data) => {
+      let chatBox = document.querySelector("#chat-box");
+  
+      let chatSendBox = document.createElement("div");
+      chatSendBox.className = "outgoing_msg";
+      chatSendBox.innerHTML = getSendMsgBox(data);
+      chatBox.append(chatSendBox);
+  
+      document.documentElement.scrollTop = document.body.scrollHeight;
+    }
+  
+    const initYourMessage = (data) => {
+      let chatBox = document.querySelector("#chat-box");
+  
+      let chatReceivedBox = document.createElement("div");
+      chatReceivedBox.className = "incoming_msg";
+      chatReceivedBox.innerHTML = getReceiveMsgBox(data);
+      chatBox.append(chatReceivedBox);
+  
+      document.documentElement.scrollTop = document.body.scrollHeight;
+    }
+  
+    const addMessage = async () => {
+      const msgInput = document.querySelector("#chat-outgoing-msg");
+      const chat = {
+        sender: nickname,
+        roomNum: chatRoomId,
+        msg: msgInput.value
+      };
+  
+      await axios.post("http://localhost:8080/chat", {
+        method: "post",
+        body: JSON.stringify(chat),
+        headers: {
+          "Content-Type": "application/json;charset=utf8"
+        }
+      });
+  
+      msgInput.value = "";
+    };
+  
+    const getSendMsgBox = (data) => {
+      let md = data.createdAt.substring(5,10);
+      let tm = data.createdAt.substring(11,16);
+      let convertTime = md+ " | "+tm;
+  
+      return `<div class="sent_msg"> <p>${data.msg}</p> <span class="time_date">${convertTime}</span> </div>`;
+    }
+  
+    const getReceiveMsgBox = (data) => {
+      let md = data.createdAt.substring(5,10);
+      let tm = data.createdAt.substring(11,16);
+      let convertTime = md+ " | "+tm;
+  
+      return `<p style="font-size: 14px;">${data.sender}</p><div class="received_withd_msg"> <p>${data.msg}</p> <span class="time_date">${convertTime}</span> </div>`;
+  }
+  
+  document.querySelector("#chat-send").addEventListener("click",() =>{
+    addMessage();
+  });
+  
+  
+  document.querySelector("#chat-outgoing-msg").addEventListener("keydown",(e) =>{
+    console.log(e.keyCode);
+    if(e.keyCode === 13){
+        addMessage();
+    }
+  });
+
+  //=====
     const eventSource = new EventSource(`http://localhost:8080/chat/roomNum/${chatRoomId}`);
     
     eventSource.onmessage = (event) => {
       const data = JSON.parse(event.data);
-      setMessages(prevMessages => [...prevMessages, data]);
-    };
+
+      if(data.sender === nickname){ // 로그인한 유저가 보낸 메세지
+        // 파란 박스(오른쪽)
+        initMyMessage(data);
+    }
+    else{
+        //회색 박스(왼쪽)
+        initYourMessage(data);
+    }
+  };
     
     return () => {
       eventSource.close(); // WebFlux 연결 해제
     };
   }, [chatRoomId]);
 
-  const addMessage = async () => {
-    const msgInput = document.querySelector("#chat-outgoing-msg");
-    const chat = {
-      sender: nickname,
-      roomNum: chatRoomId,
-      msg: msgInput.value
-    };
-
-    await axios.post("http://localhost:8080/chat", {
-      method: "post",
-      body: JSON.stringify(chat),
-      headers: {
-        "Content-Type": "application/json;charset=utf8"
-      }
-    });
-
-    msgInput.value = "";
-  };
-
-  return (
-    <>
-     <div className="flex items-center p-4 border-b border-gray-300">
-        <div className="flex flex-col">
-          <span className="font-semibold">John Doe</span>
+return (
+  <>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-12">
+          <div id="user_chat_data" className="user_chat_data">
+            <div className="profile_name">
+             
+              <span id="nickname"></span>
+            </div>
+            <div className="container-fluid chat_section" id="chat-box"></div>
+            <div className="type_msg">
+              <div className="input_msg_write">
+                <input id="chat-outgoing-msg" type="text" className="write_msg" placeholder="Type a message" />
+                <button id="chat-send" className="msg_send_btn" type="button">
+                  <i className="fa fa-paper-plane" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    <div>
-    <MessageList messages={messages} username={nickname} />
-    <MessageInput onSendMessage={addMessage} />
     </div>
 
-    </>
-  );
-};
+    {/* jQuery */}
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+      crossOrigin="anonymous"></script>
 
-const MessageList = ({ messages, username }) => (
-  <div id="chat-box">
-    {messages.map((message, index) => (
-      <Message key={index} message={message} isOwnMessage={message.sender === username} />
-    ))}
-  </div>
+    {/* Popper.js */}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+      integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+      crossOrigin="anonymous"></script>
+
+    {/* Bootstrap JS */}
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+      integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+      crossOrigin="anonymous"></script>
+  </>
 );
 
-const Message = ({ message, isOwnMessage }) => {
-  const md = message.createdAt.substring(5, 10);
-  const tm = message.createdAt.substring(11, 16);
-  const convertTime = `${md} | ${tm}`;
-
-  return (
-    <div className={isOwnMessage ? "outgoing_msg" : "incoming_msg"}>
-      {isOwnMessage ? (
-        <div className="sent_msg">
-          <p>{message.msg}</p>
-          <span className="time_date">{convertTime}</span>
-        </div>
-      ) : (
-        <>
-          <p style={{ fontSize: '14px' }}>{message.sender}</p>
-          <div className="received_withd_msg">
-            <p>{message.msg}</p>
-            <span className="time_date">{convertTime}</span>
-          </div>
-        </>
-      )}
-    </div>
-  );
 };
 
-const MessageInput = ({ onSendMessage }) => (
-  <div>
-    <input id="chat-outgoing-msg" type="text" onKeyDown={(e) => { if (e.keyCode === 13) onSendMessage(); }} />
-    <button id="chat-send" onClick={onSendMessage}>Send</button>
-  </div>
-);
 
 export default ChatApp;
