684fd3ca40992ee4f01abdd73f913d4c40689a16
gnsgh0323 <137294567+gnsgh0323@users.noreply.github.com>
2023-12-13 10:43:01 +0900

    .

--- src/components/StudyRoom/StudyModify.jsx
+++ src/components/StudyRoom/StudyModify.jsx
@@ -288,7 +288,7 @@ export default function StudyModify() {
                         <div className="flex gap-x-3">
                             <button
                                 type="reset"
-                                onClick={() => navigate('/studyroom/manage')}
+                                onClick={() => navigate(`/studyroom/${study_id}/manage`)}
                                 className="flex-1 rounded-md bg-red-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                             >
                                 취소
