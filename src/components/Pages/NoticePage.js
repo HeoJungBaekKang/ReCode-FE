b49439d69c74cdc8c04bc3cf5157c73ca5d6c5ae
minhee <minhee810@naver.com>
2023-12-16 20:33:43 +0900

    공지사항 페이징 수정

--- src/components/Pages/NoticePage.js
+++ src/components/Pages/NoticePage.js
@@ -33,7 +33,7 @@ export default function NoticePage() {
     try {
       const response = await fetchNoticeList(currentPage); // API에서 공지사항 목록을 가져오는 함수
       setDisplayList(response.data); // 가져온 데이터를 상태에 설정
-      
+
       console.log(response.data);
     } catch (error) {
       console.log("목록 불러오기 실패", error);
@@ -42,7 +42,6 @@ export default function NoticePage() {
 
   // 키워드 검색 컴포넌트 핸들러 검색 결과 출력  result 에 검색 결과 담김
   const handleSearch = async (searchType, searchTerm) => {
- 
     const response = await handleSearchKeyword(
       searchType,
       searchTerm,
@@ -146,54 +145,6 @@ export default function NoticePage() {
                 </button>
               </Link>
             )}
-          </div>
-          <div>
-            <div className="mt-6 flex justify-center">
-              <nav aria-label="Page navigation example">
-                <ul className="list-style-none flex">
-                  <li key="previous-button">
-                    <button
-                      disabled={currentPage === 0}
-                      onClick={() => setCurrentPage(currentPage - 1)}
-                      className={`relative block rounded bg-transparent px-3 py-1.5 text-sm ${
-                        currentPage === 0
-                          ? "text-neutral-500"
-                          : "text-neutral-600"
-                      } transition-all duration-300 dark:text-neutral-400`}
-                    >
-                      Previous
-                    </button>
-                  </li>
-                  {chunkedPosts.map((_, index) => (
-                    <li key={`page-button-${index}`}>
-                      <button
-                        onClick={() => setCurrentPage(index)}
-                        className={`flex flex-col cursor-pointer items-center justify-center w-9 h-9 shadow-[0_4px_10px_rgba(0,0,0,0.03)] text-sm font-normal transition-colors rounded-lg ${
-                          index === currentPage
-                            ? "bg-gray-300 text-neutral-600"
-                            : "bg-gray-100 text-neutral-600"
-                        } transition-all duration-300 dark:text-black dark:hover:bg-neutral-700 dark:hover:text-white`}
-                      >
-                        {index + 1}
-                      </button>
-                    </li>
-                  ))}
-                  <li key="next-button">
-                    <button
-                      disabled={currentPage === chunkedPosts.length - 1}
-                      onClick={() => setCurrentPage(currentPage + 1)}
-                      className={`relative block rounded bg-transparent px-3 py-1.5 text-sm ${
-                        currentPage === chunkedPosts.length - 1
-                          ? "text-neutral-600"
-                          : "text-neutral-600"
-                      } transition-all duration-300 dark:text-neutral-400`}
-                    >
-                      Next
-                    </button>
-                  </li>
-                </ul>
-              </nav>
-            </div>
           </div>
         </div>
         <div className="mt-6 flex justify-center">
