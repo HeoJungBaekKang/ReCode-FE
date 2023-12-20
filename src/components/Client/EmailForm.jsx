311f46c5f7f878bf79670ab721b37b145f08185f
백승주 <137259484+Oriduckquak@users.noreply.github.com>
2023-12-21 00:14:44 +0900

    이메일 폼 수정

--- src/components/Client/EmailForm.jsx
+++ src/components/Client/EmailForm.jsx
@@ -44,7 +44,9 @@ export default function Email() {
             return null;
         }
     }
-    const handleOnClick = async () => {
+    const handleSubmitForm = async (e) => {
+        e.preventDefault();
+
         const emailExists = await checkEmailExist();
         if (emailExists === -1) {
             await handleSubmit();
@@ -58,7 +60,7 @@ export default function Email() {
     return (
         <div className="flex justify-center items-center h-screen mt-[-5rem]">
             <div className="w-full max-w-xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-                <form className="space-y-6" action="#">
+                <form className="space-y-6" onSubmit={handleSubmitForm}>
                     <h3 className="text-xl font-medium text-gray-900 dark:text-white">Recode</h3>
                     {loading && (
                         <div
@@ -98,7 +100,6 @@ export default function Email() {
                         </button>
                         <button
                             type="submit"
-                            onClick={handleOnClick}
                             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         >
                             이메일 전송
